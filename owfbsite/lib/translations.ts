8e13c40a57c3f48bb3964949f413c200578d88ea
Urtep69 <brionelv@gmail.com>
2025-08-11 01:53:26 +0200

    Delete owfbsite directory

--- owfbsite/lib/translations.ts
+++
@@ -1,5 +0,0 @@
-// This file is deprecated.
-// Translations have been moved to individual files in the lib/locales/ directory.
-// This file will be removed in a future update.
-
-export const translations: Record<string, Record<string, string>> = {};
