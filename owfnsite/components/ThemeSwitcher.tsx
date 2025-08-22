4769689caa9a0ada8c66a0f27e2f6ac9ebbe53b3
Urtep <brionelv@gmail.com>
2025-08-22 18:35:39 +0200

    Delete owfnsite/components directory

--- owfnsite/components/ThemeSwitcher.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// This component is no longer used as the theme is permanently set to dark.
-// It is kept as an empty component to prevent import errors in other files
-// that might still reference it before a full cleanup.
-export const ThemeSwitcher = () => {
-    return null; 
-};
