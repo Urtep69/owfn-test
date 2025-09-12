32fffd29eae4a317836b616dc2f5394f2e6e01ba
Urtep <brionelv@gmail.com>
2025-09-12 11:33:48 +0200

    Delete owfnsite/lib directory

--- owfnsite/lib/assets.ts
+++
@@ -1,4 +0,0 @@
-export const owfnLogo = '/assets/owfn-coin.png';
-export const solanaLogo = '/assets/solana.png';
-export const usdcLogo = '/assets/usdc.png';
-export const usdtLogo = '/assets/usdt.png';
