942bad4622871172af44ff91a2d24f05f950adb2
Urtep <brionelv@gmail.com>
2025-09-16 22:22:32 +0200

    Delete owfnsite/components directory

--- owfnsite/components/SkeletonLoader.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const SkeletonLoader = ({ className = '' }: { className?: string }) => (
-    <div className={`bg-primary-200 dark:bg-darkPrimary-700 rounded-md animate-pulse ${className}`} />
-);
